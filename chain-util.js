2553698a8dada50d58166b7a8fd32b7a947e28ca
Cristian Buta <cristian.buta@arnia.ro>
2021-02-28 10:53:09 +0200

    Wallets and Transactions on the blockchain

--- chain-util.js
+++ chain-util.js
@@ -1,5 +1,6 @@
 const EC = require('elliptic').ec;
 const uuidV1 = require('uuid').v1;
+const SHA256 = require('crypto-js/sha256');
 const ec = new EC('secp256k1');
 
 class ChainUtil {
@@ -7,9 +8,17 @@ class ChainUtil {
         return ec.genKeyPair();
     };
 
-    static id(){
+    static id() {
         return uuidV1();
-    }
+    };
+
+    static hash(data) {
+        return SHA256(JSON.stringify(data)).toString();
+    };
+
+    static verifySignature(publicKey, signature, dataHash) {
+        return ec.keyFromPublic(publicKey, 'hex').verify(dataHash, signature);
+    };
 }
 
 module.exports = ChainUtil;
