66a8cbd5fccfcc7d440c1c2df1530bc20b7679a8
Cristian Buta <cristian.buta@arnia.ro>
2021-02-28 23:24:19 +0200

    Enhanced wallet and added more tests

--- app/index.js
+++ app/index.js
@@ -2,10 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Blockchain = require('../blockchain');
 const P2pServer = require('./p2p-server');
+const Wallet = require('../wallet');
+const TransactionPool = require('../wallet/transaction-pool');
 
 const HTTP_PORT = process.env.HTTP_PORT || 3001;
 
 const app = express();
+const wallet = new Wallet();
+const tp = new TransactionPool();
 const bc = new Blockchain();
 const p2pServer = new P2pServer(bc);
 
@@ -15,13 +19,23 @@ app.get('/blocks', (req, res) => {
     res.json(bc.chain);
 });
 
+app.get('/transactions', (req, res) => {
+    res.json(tp.transactions)
+});
+
 app.post('/mine', (req, res) => {
     const block = bc.addBlock(req.body.data);
     console.log(`New block added: ${block.toString()}`)
-    
+
     p2pServer.syncChains();
 
     res.redirect('/blocks')
+});
+
+app.post('/transact', (req, res) => {
+    const { recipient, amount } = req.body;
+    const transaction = wallet.createTransaction(recipient, amount, tp);
+    res.redirect('transactions');
 });
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}`));
